74a6830550f0c484922b1b3ee64a3166c9368904
Jacques Malgrange <jacques.malgrange@gmail.com>
2023-03-07 11:40:04 +0100

    V1.3.3

--- video_playerMake.php
+++ video_playerMake.php
@@ -2,11 +2,11 @@
 if(!isset($_SESSION['cmsuno'])) exit();
 ?>
 <?php
-if(strpos($Ucontent, 'class="videoPlayer"')!==false)
-	{
+if(strpos($Ucontent, 'class="videoPlayer"')!==false) {
 	$Ufoot .= '<script type="text/javascript" src="uno/plugins/video_player/videojs/video.min.js"></script>'."\r\n";
 	$Ufoot .= '<script type="text/javascript">videojs.options.flash.swf="uno/plugins/video_player/videojs/video-js.swf";</script>'."\r\n";
 	$Uhead .= '<link href="uno/plugins/video_player/videojs/video-js.css" rel="stylesheet">'."\r\n";
-	$Ustyle .= '.video-js .vjs-control-bar{display:flex;}'."\r\n";
-	}
+	$Ustyle .= '.video-js .vjs-control-bar{display:flex;}';
+}
+$Ustyle .= 'div.embeddedContent iframe{max-width:100%;}'."\r\n";
 ?>
